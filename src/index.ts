a09a4b792fce1f0c1b6045b94cff7ccaa32f3d63
ShreddedPork <r.ho@miami.edu>
2021-01-09 21:57:48 -0500

    added ENV file, added JOI validation, added MongoDB

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,39 @@
 'use strict';
 
+require('dotenv').config();
+
+
 const Hapi = require('@hapi/hapi');
 const Path = require('path');
+const Joi = require('joi');
+const mongoose = require('mongoose');
+
+const schema = Joi.object({
+    userWeight: Joi.number()
+        .required(),
+    userHeight: Joi.number()
+        .required(),
+    userName: Joi.string()
+        .required()
+})
 
 const start = async () => {
+
+    mongoose.connect(process.env.MONGO, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
+
+    const userSchema = {
+        user: String,
+        bmi: Number,
+        added: Date
+    };
+
+    
+
+    const users = mongoose.model('userBMI', userSchema);
+
 
     const server = Hapi.server({
         port: 3000,
@@ -46,15 +76,31 @@ const start = async () => {
     });
 
 
+    
+
     server.route({
         method: 'POST',
         path: '/bmicalculator',
         handler: function (request, h) {
             let userWeight = parseFloat(request.payload.weight);
             let userHeight = parseFloat(request.payload.height);
+            let userName = request.payload.username;
             let bmi = Math.round(userWeight / (userHeight * userHeight));
-            return `your bmi is ${bmi}`;
+            // return `hey ${userName} your bmi is ${bmi}`;
+
+           
+
+
+            const user = new users({
+                user: userName,
+                bmi: bmi,
+                added: new Date()
+            });
+            user.save();
+            return h.redirect('/bmicalculator');
+
         }
+        
     });
 
     await server.start();
